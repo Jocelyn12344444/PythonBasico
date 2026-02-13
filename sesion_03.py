119ab5cbcf192e70d135f3d2ea398f682bda1588
Jocelyn12344444 <joselynmartinezurcino@gmail.com>
2026-02-12 17:33:02 -0800

    Elabore la practica 2 donde teniamos que ejecutar los dias de la semana

--- sesion_03.py
+++ sesion_03.py
@@ -1,21 +1,62 @@
-# Loops
+#Loops
 
 mi_lista = [1,2,3,4,5]
 
 for i in mi_lista:
-    print("El numero es:", i)
+    print("elnumero es:", i)
 
-resultado = 0 
+resultado = 0
 for i in mi_lista:
     resultado += i
 
-print(f"El resultado de la suma de mi lista es: {resultado}")
+print(f"el resultado de la suma de la lista es: {resultado}")
 
-for i in range(2, 9):
+for i in range(2,8):
     print(i)
 
-mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
-
+mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
 for i in mi_lista_2:
-    if i != "lunes":
-        print(f"FELIZ {i}")
+    if i == "lunes":
+        print(f"feliz{i}¡")
+
+
+# while loop
+i = 0
+
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    if i == 4:
+        break
+
+else:
+    print("i es ahora mayor o igual a 5")
+
+#practica 2
+# Dada la lista mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
+#inprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# Pista: usa los dos tipos loops while y foren el mismo programa para lograrlo
+# resultado:
+# martes
+# miercoles
+# jueves
+# viernes
+# martes
+# miercoles
+# jueves
+# viernes
+# martes
+# miercoles
+# jueves
+# viernes
+
+mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
+
+for dia in mi_lista_2:
+    if dia != "lunes":
+        contador = 0
+        while contador < 3:
+            print(dia)
+            contador += 1
